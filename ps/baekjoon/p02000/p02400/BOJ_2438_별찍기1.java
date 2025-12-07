37b007086571d130fc410d28ae912f4c01b0853a
tecte <tecte1023@gmail.com>
2025-12-08 04:34:29 +0900

    [BOJ] 2438 별 찍기 - 1

---
+++ ps/baekjoon/p02000/p02400/BOJ_2438_별찍기1.java
@@ -0,0 +1,31 @@
+/*
+ * 문제: 백준 2438번 - 별 찍기 - 1 (https://www.acmicpc.net/problem/2438)
+ *
+ * 시간 복잡도: O(n²)
+ */
+
+package baekjoon.p02000.p02400;
+
+import java.io.*;
+
+public class BOJ_2438_별찍기1 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append('*');
+            }
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+}
