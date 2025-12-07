6f538179307a509d4c4b598e914afa608f539155
tecte <tecte1023@gmail.com>
2025-12-08 02:55:05 +0900

    [BOJ] 10172 개

---
+++ ps/baekjoon/p10000/p10100/BOJ_10172_개.java
@@ -0,0 +1,23 @@
+/*
+ * 문제: 백준 10172번 - 개 (https://www.acmicpc.net/problem/10172)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.p10000.p10100;
+
+import java.io.*;
+
+public class BOJ_10172_개 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+        bw.close();
+    }
+}
