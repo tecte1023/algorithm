8167dcf02c48ad65655c119eed80933ce6307230
tecte <tecte1023@gmail.com>
2025-12-08 02:06:51 +0900

    [BOJ] 2557 Hello World

---
+++ ps/baekjoon/p02000/p02500/BOJ_2557_HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * 문제: 백준 2557번 - Hello World (https://www.acmicpc.net/problem/2557)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.p02000.p02500;
+
+import java.io.*;
+
+public class BOJ_2557_HelloWorld {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+        bw.close();
+    }
+}
