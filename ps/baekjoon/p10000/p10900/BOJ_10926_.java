1d7360a7169835029e7bb137f5cfbec5511e223d
tecte <tecte1023@gmail.com>
2025-12-08 02:26:36 +0900

    [BOJ] 10869 사칙연산

---
+++ ps/baekjoon/p10000/p10900/BOJ_10926_.java
@@ -0,0 +1,21 @@
+/*
+ * 문제: 백준 10926번 - ??! (https://www.acmicpc.net/problem/10926)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.p10000.p10900;
+
+import java.io.*;
+
+public class BOJ_10926_ {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine() + "??!");
+        br.close();
+        bw.close();
+    }
+}
