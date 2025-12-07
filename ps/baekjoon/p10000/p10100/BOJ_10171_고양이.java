47bc5f386f01a461546857bd81377b990fe33218
tecte <tecte1023@gmail.com>
2025-12-08 02:53:43 +0900

    [BOJ] 10171 고양이

---
+++ ps/baekjoon/p10000/p10100/BOJ_10171_고양이.java
@@ -0,0 +1,22 @@
+/*
+ * 문제: 백준 10171번 - 고양이 (https://www.acmicpc.net/problem/10171)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.p10000.p10100;
+
+import java.io.*;
+
+public class BOJ_10171_고양이 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+        bw.close();
+    }
+}
