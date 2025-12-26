9b032c8c6b5ff8b1a48891c58ef3f4cfb68091c3
tecte <tecte1023@gmail.com>
2025-12-26 19:04:08 +0900

    [BOJ] 1978 소수 찾기

---
+++ ps/baekjoon/p01000/p01900/BOJ_1978_소수찾기.java
@@ -0,0 +1,49 @@
+/*
+ * 문제: 백준 1978번 - 소수 찾기 (https://www.acmicpc.net/problem/1978)
+ * 문제 풀이
+ *
+ *
+ * 시간 복잡도:
+ */
+
+package baekjoon.p01000.p01900;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1978_소수찾기 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (isPrime(num)) {
+                cnt++;
+            }
+        }
+
+        bw.write(Integer.toString(cnt));
+        br.close();
+        bw.close();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
