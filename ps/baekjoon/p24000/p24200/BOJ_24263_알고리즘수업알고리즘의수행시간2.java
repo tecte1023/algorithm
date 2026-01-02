086bf2f7c06a69711202a41d7182d228bafa27ea
tecte <tecte1023@gmail.com>
2026-01-03 05:47:48 +0900

    [BOJ] 24263 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ ps/baekjoon/p24000/p24200/BOJ_24263_알고리즘수업알고리즘의수행시간2.java
@@ -0,0 +1,21 @@
+/*
+ * 문제: 백준 24263번 - 알고리즘 수업 - 알고리즘의 수행 시간 2 (https://www.acmicpc.net/problem/24263)
+ *
+ * 시간 복잡도: O(1)
+ */
+
+package baekjoon.p24000.p24200;
+
+import java.io.*;
+
+public class BOJ_24263_알고리즘수업알고리즘의수행시간2 {
+    // public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine() + "\n1");
+        br.close();
+        bw.close();
+    }
+}
