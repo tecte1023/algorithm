ed4c2b0929fed81a33376d463ee8ad26ea11a9e0
tecte <tecte1023@gmail.com>
2026-02-17 12:07:10 +0900

    [BOJ] 24445 알고리즘 수업 - 너비 우선 탐색 2

---
+++ ps/baekjoon/p24000/p24400/BOJ_24445_알고리즘수업너비우선탐색2.java
@@ -0,0 +1,77 @@
+/*
+ * 문제: 백준 24445번 - 알고리즘 수업 - 너비 우선 탐색 2 (https://www.acmicpc.net/problem/24445)
+ * 문제 풀이
+ *
+ *
+ * 시간 복잡도:
+ */
+
+package baekjoon.p24000.p24400;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_24445_알고리즘수업너비우선탐색2 {
+    // public class Main {
+    private static final List<PriorityQueue<Integer>> graph = new ArrayList<>();
+    private static int[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new PriorityQueue<>(Comparator.reverseOrder()));
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).offer(v);
+            graph.get(v).offer(u);
+        }
+
+        visited = new int[n + 1];
+        bfs(r);
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+
+    private static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+        int order = 1;
+
+        queue.offer(node);
+        visited[node] = order++;
+
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+
+            PriorityQueue<Integer> pq = graph.get(node);
+
+            while (!pq.isEmpty()) {
+                int next = pq.poll();
+
+                if (visited[next] != 0) {
+                    continue;
+                }
+
+                queue.offer(next);
+                visited[next] = order++;
+            }
+        }
+    }
+}
